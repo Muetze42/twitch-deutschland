f1f0abc4811c88bc4e85a0e827b6aef688403297
Norman Huth <norman@huth.it>
2022-01-21 23:10:32 +0100

    Update dependencies and change mobile css

--- app/Http/Controllers/BroadcasterController.php
+++ app/Http/Controllers/BroadcasterController.php
@@ -18,14 +18,16 @@ class BroadcasterController extends Controller
             ->whereHas('videos')
             ->ordered()
             ->when(Request::input('search'), function ($query, $search) {
-                $query->where('name', 'like', '%'.$search.'%')
-                    ->orWhere('display_name', 'like', '%'.$search.'%');
+                $query->where(function ($query) use ($search) {
+                    $query->where('name', 'like', '%'.$search.'%')
+                        ->orWhere('display_name', 'like', '%'.$search.'%');
+                });
             })
             ->paginate($this->limit)
             ->withQueryString()
             ->through(fn($broadcaster) => [
                 'id'    => $broadcaster->id,
-                'first' => $broadcaster->videos()->first()->youtube_id,
+                'first' => $broadcaster->videos()->first() ? $broadcaster->videos()->first()->youtube_id : null,
                 'name'  => e($broadcaster->display_name),
                 'logo'  => $broadcaster->logo,
                 'count' => number_format($broadcaster->video_count, 0, ',', '.'),
